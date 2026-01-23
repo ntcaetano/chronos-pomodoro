d3f5f84f89d6d76b5a1a0650985b3b4c55828101
Natanael Caetano <natacaet@gmail.com>
2026-01-23 19:58:32 -0300

    commit
    Aula 20-25

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import { TimerIcon } from 'lucide-react';
-import { Heading } from './components/Heading';
-import './styles/global.css'
-import './styles/theme.css'
+import { Container } from "./components/Container";
+import { Heading } from "./components/Heading";
+import { Logo } from "./components/Logo";
+
+import "./styles/global.css";
+import "./styles/theme.css";
 
 export function App() {
-
-    return (
-        <>
-            <Heading>
-                Olá Mundo 1
-                <button>
-                    <TimerIcon />
-                </button>
-            </Heading>
-    
-            <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio porro et deserunt voluptates sint repellendus error expedita aperiam explicabo! Magni fugit quas, tempore fuga magnam voluptas maxime eum eius recusandae.
-            </p>
-        </>
-    );
+  return (
+    <>
+      <Container>
+        <Logo />
+      </Container>
+      
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
+    </>
+  );
 }
