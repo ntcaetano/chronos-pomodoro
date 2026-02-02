0e8e841247a2dcf6285b2d9d30d4fc01a17548d9
Natanael Caetano <natacaet@gmail.com>
2026-02-02 13:19:05 -0300

    Commit
    Aula 31/32

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { PlayCircleIcon, StopCircleIcon } from "lucide-react";
+import { PlayCircleIcon } from "lucide-react";
 import { Container } from "./components/Container";
 import { CountDown } from "./components/CountDown";
 import { Cycles } from "./components/Cycles";
@@ -9,6 +9,7 @@ import { Menu } from "./components/Menu";
 
 import "./styles/global.css";
 import "./styles/theme.css";
+import { Footer } from "./components/Footer";
 
 export function App() {
   return (
@@ -45,10 +46,13 @@ export function App() {
           </div>
 
           <div className="formRow">
-            <DefaultButton icon={<PlayCircleIcon />} color="green" />
-            <DefaultButton icon={<StopCircleIcon />} color="red" />
+            <DefaultButton icon={<PlayCircleIcon />} />
           </div>
         </form>
+      </Container>
+
+      <Container>
+        <Footer />
       </Container>
     </>
   );
